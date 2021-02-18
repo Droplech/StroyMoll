dcd689afbbb51f4a82ba6336aa81dcc8ec48c02c
Maxim <53229586+Droplech@users.noreply.github.com>
2021-02-18 22:07:40 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -23,17 +23,62 @@ $(document).ready(function(){
 })
 //-tabs
 
+$('.nav_page_li').click(function(){
+    $('.nav_page_li').removeClass('li-active')
+    $(this).addClass('li-active')
+})
 
 
 
+$(".item_filter .title").click(function(){
+    if( !$(this).hasClass('active')){
+        $(this).addClass('active')
+        $(this).next().slideDown()
+        $(this).find('.icon').css({
+            'transform':'rotate(180deg)',
+            'transition':'.3s'
+        })
+        
+    }else{
+        $(this).removeClass('active')
+        $(this).next().slideUp()
+        $(this).find('.icon').css({
+            'transform':'rotate(0deg)',
+            'transition':'.3s'
+        })
+        
+    }
+})
+
+
+$('.label .txt').click(function(){
+    if( !$(this).hasClass('label-active') ){
+        $(this).addClass('label-active')
+        
+    }else{
+        $(this).removeClass('label-active')
+        
+
+    }
+
+    
+})
 
 
 
-
-
-
-
-
+$('.btn_moreBrands').click(function(e){
+    e.preventDefault()
+    if( !$('.btn_moreBrands').hasClass('active') ){
+        $('.btn_moreBrands').addClass('active')
+        $('.more_brands').slideDown()
+        $('.btn_moreBrands').html('Скрыть')
+    }else{
+        $('.btn_moreBrands').removeClass('active')
+        $('.more_brands').slideUp()
+        $('.btn_moreBrands').html('Показать еще')
+    }
+    
+})
 
 
 
