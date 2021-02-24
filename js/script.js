5bae45a68229741c68274f07a2e0eb4f5c992091
Maxim <53229586+Droplech@users.noreply.github.com>
2021-02-24 20:00:19 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //+tabs
 $('.col_product ul li').hover(function(e){
+    e.preventDefault()
     $('.col_product ul li').removeClass('active')
     $('.cat_detail').removeClass('cat_detail_active')
 
@@ -22,6 +23,12 @@ $(document).ready(function(){
     
 })
 //-tabs
+
+$('.shop_tab').click(function(e){
+    e.preventDefault()
+    $('.shop_tab').removeClass('a_active')
+    $(this).addClass('a_active')
+})
 
 $('.nav_page_li').click(function(){
     $('.nav_page_li').removeClass('li-active')
@@ -81,10 +88,117 @@ $('.btn_moreBrands').click(function(e){
 })
 
 
+$('.mob_burg').click(function(){
+    $('.mob_burg').addClass('active')
+    $('.mob_menu_content').fadeIn()
+    
+})
+$('.closed').click(function(){
+    
+    $('.mob_menu_content').fadeOut()
+    
+})
+
+if ( $(window).width() < 1050 ){
+    $('.col_product ul li a').click(function(e){
+        e.preventDefault()
+    })
+}
+
+if ( $(window).width() < 500 ){
+
+    $('.col_product ul li').removeClass('active')
+
+
+
+    $('.col_product ul li a').click(function(e){
+        e.preventDefault()
+        $('.col_detail_block').css({
+            'right':'0',
+            'transition':'.3s'
+        })
+    })
+
+    $('.close_detail').click(function(){
+        $('.col_detail_block').css({
+            'right':'-388px',
+            'transition':'.3s'
+        })
+    })
+
+
+    
+}
 
 
 
 
+$('.open_filter').click(function(e){
+    e.preventDefault()
+   if( !$('.open_filter').hasClass('active') ){
+        $('.open_filter').addClass('active')
+        $('.open_filter').html('Закрыть фильтр')
+        $('.filter').fadeIn()
+   }else{
+    $('.open_filter').removeClass('active')
+    $('.open_filter').html('Фильтр')
+    $('.filter').fadeOut()
+   }
+})
+
+
+$('.slider-nav .item').click(function(){
+    $('.slider-nav .item').removeClass('item_border-active')
+    $(this).addClass('item_border-active')
+})
+
+
+
+
+
+const counter = (selector, action) => {
+    let price = selector.parents('.basket_product').find($('.count_info'));
+  
+    switch (action) {
+      case 'increment': 
+        price.val(parseInt(price.val()) + 1); break;
+      case 'decrement': 
+        if (price.val() < 1) { price.val(0) } else { price.val(parseInt(price.val()) - 1);} break;  
+    }
+  } 
+  $('.count_plus').click(function() {
+    counter($(this), 'increment')
+  })
+  
+  $('.count_minus').click(function() {
+    counter($(this), 'decrement')
+  })
+
+
+
+
+
+$('.slider_shop').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+})
+$('.left-shop_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+})
+$('.brand-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+})
 
 
 $('.brands_slider').slick({
@@ -93,4 +207,91 @@ $('.brands_slider').slick({
     speed: 300,
     slidesToShow: 7,
     slidesToScroll: 2,
+    responsive:[
+        {
+            breakpoint: 1690,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 770,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        }
+    ]
 });
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+$('.slider-nav').slick({
+slidesToShow: 6,
+slidesToScroll: 1,
+asNavFor: '.slider-for',
+dots: false,
+arrows:false,
+focusOnSelect: true,
+responsive:[
+    {
+    breakpoint: 420,
+    settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        
+    }
+    }
+]
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
